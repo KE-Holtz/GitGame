b7b0fefad89b4407b6c763bcaf73bff2605e211f
Kyle Holtz <keholtz08@gmail.com>
2024-06-30 20:41:57 -0400

    :

--- Main.java
+++ Main.java
@@ -3,10 +3,13 @@ import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(canPull());
+        while (isUpToDate()) {
+            System.out.println("Still up to date");
+        }
+        System.out.println("new changes");
     }
 
-    public static boolean canPull() {
+    public static boolean isUpToDate() {
         ProcessBuilder fetch = new ProcessBuilder("git", "fetch");
         byte[] bytes;
         try {
@@ -14,7 +17,6 @@ public class Main {
             bytes = new byte[inputStream.available()];
             inputStream.read(bytes);
             String result = new String(bytes);
-            result += 'c';
             Logger.log(result);
             return result.isBlank();
         } catch (IOException e) {
